dfebb1a5b93f42b11c6f1407b1dda96e181c6607
sahilshaik123 <112350606+sahilshaik123@users.noreply.github.com>
2024-01-20 11:57:52 -0500

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,21 @@
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+)
+
+from fact_checking import FactChecker
+
+import sys
+
+
+def get_confidence(context, claim):
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    fact_checking_model = GPT2LMHeadModel.from_pretrained('fractalego/fact-checking')
+    fact_checker = FactChecker(fact_checking_model, tokenizer)
+    is_claim_true = fact_checker.validate_with_replicas(context, claim)
+    return is_claim_true['Y'] * 100
+
+if __name__ == "__main__":
+    a = str(sys.argv[1])
+    b = str(sys.argv[2])
+    print(get_confidence(a, b))
